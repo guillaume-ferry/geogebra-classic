d83dad5aeb5a7599620883aba44e10fd1bf523fb
Nicolas Rogier <nicolas.rogier@univ-lorraine.fr>
2019-10-11 09:51:35 +0200

    update to version 6.0.560

--- tools/chocolateyinstall.ps1
+++ tools/chocolateyinstall.ps1
@@ -1,13 +1,13 @@
 ﻿$ErrorActionPreference = 'Stop'
-$url        = 'https://download.geogebra.org/installers/6.0/GeoGebra-Windows-Installer-6-0-559-0.msi'
-$version    = '6.0.559.0'
+$url        = 'https://download.geogebra.org/installers/6.0/GeoGebra-Windows-Installer-6-0-560-0.msi'
+$version    = '6.0.560.0'
 
 $packageArgs = @{
   packageName   = $env:ChocolateyPackageName
   fileType      = 'MSI'
   url           = $url
   softwareName  = 'GeoGebra Classic*'
-  checksum      = '3A9DE71421F42CCA033D30212996080A8DC36ABC293565904D05EEC9B27D0379'
+  checksum      = 'D2BF25F45E4247B38C242D6FB478B6646A9B0297B108C4D40629E172153EA889'
   checksumType  = 'sha256'
   silentArgs    = "ALLUSERS=2 /qn /norestart /l*v `"$($env:TEMP)\$($packageName).$($env:chocolateyPackageVersion).MsiInstall.log`""
   validExitCodes= @(0, 3010, 1641)
@@ -33,8 +33,9 @@ if ($checkreg.Count -eq 0) {
                 # Process to install
                 Write-Output "Installing new version of Geogebra Classic"
                 Install-ChocolateyPackage @packageArgs
-            } elseif ($_.DisplayVersion.Replace('.', '') -eq $InstallerVersion) {
-                Write-Output "Geogebra Classic $version already installed, skip download and install"
+            } elseif (($_.DisplayVersion.Replace('.', '') -eq $InstallerVersion) -and ($env:ChocolateyForce)) {
+                Write-Output "Geogebra Classic $version already installed, but --force option is passed, download and install"
+                Install-ChocolateyPackage @packageArgs
             }
         }
     }
