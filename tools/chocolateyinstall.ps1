636e7768fc61c7ea458d5558dc7a92b88b56cc3c
overag3 <nicolas.rogier@univ-lorraine.fr>
2020-08-18 01:13:37 +0200

    update to version 6.0.599.0

--- tools/chocolateyinstall.ps1
+++ tools/chocolateyinstall.ps1
@@ -1,13 +1,13 @@
 ﻿$ErrorActionPreference = 'Stop'
-$url        = 'https://download.geogebra.org/installers/6.0/GeoGebra-Windows-Installer-6-0-598-0.msi'
-$version    = [version]'6.0.598.0'
+$url        = 'https://download.geogebra.org/installers/6.0/GeoGebra-Windows-Installer-6-0-599-0.msi'
+$version    = [version]'6.0.599.0'
 
 $packageArgs = @{
   packageName   = $env:ChocolateyPackageName
   fileType      = 'MSI'
   url           = $url
   softwareName  = 'GeoGebra Classic*'
-  checksum      = 'bf023183dd5252f0b0ab3f0de0ae904242ca47efdefa49ccd4b0b607854a7bb5'
+  checksum      = '4c1a899bb288752fa77da5f99e7b0e5802f5be86cc06f3ad9cb026862d44aea2'
   checksumType  = 'sha256'
   silentArgs    = "ALLUSERS=2 /qn /norestart /l*v `"$($env:TEMP)\$($packageName).$($env:chocolateyPackageVersion).MsiInstall.log`""
   validExitCodes= @(0, 3010, 1641)
